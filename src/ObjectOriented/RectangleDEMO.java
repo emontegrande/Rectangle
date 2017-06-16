0753e5869f5265e1514b6a4f6d76918c5e8a6a11
emontegrande <montegrandeelouiseit1a@gmail.com>
2017-06-16 15:44:36 +0800

    Add files via upload

---
+++ src/ObjectOriented/RectangleDEMO.java
@@ -0,0 +1,13 @@
+package ObjectOriented;
+public class RectangleDEMO {
+
+    public static void main(String[] args) {
+        //class Instantiation
+        Rectangle rect1 = new Rectangle();
+        Rectangle rect2 = new Rectangle(1.2);
+        Rectangle rect3 = new Rectangle(1.2,1.4);
+        
+        rect1.displayArea();
+        rect1.display_Area();
+    }
+}
